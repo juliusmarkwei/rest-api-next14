b88f73ffcb261e9b0c4c91101054fadf89ad1dce
juliusmarkwei <juliusmarkwei2000@gmail.com>
2024-06-19 01:36:46 +0000

    changed db to remote db

--- lib/db.ts
+++ lib/db.ts
@@ -3,27 +3,28 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const connect = async () => {
-  const connectionState = mongoose.connection.readyState;
+	const connectionState = mongoose.connection.readyState;
 
-  if (connectionState === 1) {
-    console.log("Database is already connected");
-    return;
-  }
+	if (connectionState === 1) {
+		console.log("Database is already connected");
+		return;
+	}
 
-  if (connectionState === 2) {
-    console.log("Database is connecting");
-    return;
-  }
+	if (connectionState === 2) {
+		console.log("Database is connecting");
+		return;
+	}
 
-  try {
-    mongoose.connect(MONGODB_URI!, {
-      bufferCommands: true,
-    });
-    console.log("Database connected");
-  } catch (error) {
-    console.log("Database connection error", error);
-    throw new Error("Database connection error");
-  }
+	try {
+		mongoose.connect(MONGODB_URI!, {
+			dbName: "Cluster0",
+			bufferCommands: true,
+		});
+		console.log("Database connected");
+	} catch (error) {
+		console.log("Database connection error", error);
+		throw new Error("Database connection error");
+	}
 };
 
 export default connect;
